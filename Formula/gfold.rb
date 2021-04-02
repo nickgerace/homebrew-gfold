5568385e8b70866bf102f24089719de46c35796a
Nick <nickagerace@gmail.com>
2021-04-02 17:20:07 -0400

    Update to 1.0.3

--- Formula/gfold.rb
+++ Formula/gfold.rb
@@ -1,8 +1,8 @@
 class Gfold < Formula
   desc "CLI tool to help keep track of your Git repositories, written in Rust"
   homepage "https://nickgerace.dev"
-  url "https://github.com/nickgerace/gfold/archive/1.0.2.tar.gz"
-  sha256 "f1ee8ba640fa81a1e5e858215b2d473395d27b04ce4a59131240be4d4d368ca3"
+  url "https://github.com/nickgerace/gfold/archive/1.0.3.tar.gz"
+  sha256 "83cb2830a0d3e6607efe4804aaa92f4da71e99f74697230b491fa888cde79716"
   license "Apache-2.0"
 
   depends_on "rust" => :build
@@ -10,7 +10,7 @@ class Gfold < Formula
   uses_from_macos "zlib"
 
   def install
-    system "cargo", "install", "--bin", "gfold", *std_cargo_args
+    system "cargo", "install", "gfold", "--locked", "--root", *std_cargo_args[2], "--path", "./gfold"
   end
 
   test do
