77b731c0b09b3c6959c819b9d4c3d10ae8f89291
Nick Gerace <nickagerace@gmail.com>
2021-05-23 00:52:58 -0400

    Update to 1.2.1

--- Formula/gfold.rb
+++ Formula/gfold.rb
@@ -1,8 +1,8 @@
 class Gfold < Formula
   desc "CLI tool to help keep track of your Git repositories, written in Rust"
   homepage "https://nickgerace.dev"
-  url "https://github.com/nickgerace/gfold/archive/1.2.0.tar.gz"
-  sha256 "faac149fe83f8add61ef66e52e1e4b2d655644ccd2db45904f083f5398c7d079"
+  url "https://github.com/nickgerace/gfold/archive/1.2.1.tar.gz"
+  sha256 "d56b58c8685bc947965e3754ac9f7d12e542c88a1bf3d0d71803623f6fb428e1"
   license "Apache-2.0"
 
   depends_on "rust" => :build
